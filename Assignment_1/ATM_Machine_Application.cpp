38c4666b7decd6799bda4dee091513d3f5d9caae
akshat-7021-ca-chitkara <akshat7021.ca23@chitkara.edu.in>
2024-03-12 11:07:11 +0530

    Adding comments to assignment 1

--- Assignment_1/ATM_Machine_Application.cpp
+++ Assignment_1/ATM_Machine_Application.cpp
@@ -13,6 +13,21 @@ class Bank
     string password;
 
 public:
+    /**
+
+
+    Function: start
+
+
+    Description: This function is the starting point of the Indi ATM program. It displays a welcome message and a menu of options for the user to choose from. It then takes the user's input and performs the corresponding action based on the selected option.
+
+
+    Parameters: None
+
+
+    Returns: None
+    */
+
     void start()
     {
         cout << "**********Welcome To Indi ATM**********" << endl
@@ -39,12 +54,32 @@ public:
             }
         } while (option != 'q');
     }
+    /**
+     * Function: printIntroMenu
+     *
+     * Description: Prints the introductory menu options for the user.
+     *
+     * Parameters: None
+     *
+     * Returns: None
+     */
     void printIntroMenu()
     {
         cout << "Press 'l' for Login" << endl
              << "Press 'c' for Create New Account" << endl
              << "Press 'q' for Quit" << endl;
     }
+    /**
+     * Function: printMainMenu
+     * -----------------------
+     * Displays the main menu options for a banking system.
+     * The user can choose to deposit money, withdraw money,
+     * request balance, or quit the program.
+     *
+     * Parameters: None
+     *
+     * Returns: None
+     */
     void printMainMenu()
     {
         cout << "Press 'd' for Deposit Money" << endl
@@ -52,6 +87,14 @@ public:
              << "Press 'r' for Request Balance" << endl
              << "Press 'q' for quit" << endl;
     }
+    /**
+     * Function: create_new_account
+     * ---------------------------
+     * Prompts the user to enter their user id and password, and creates a new account with the entered information.
+     * The new account is stored in the ids, passwords, and balances arrays.
+     * The num_users variable is incremented to keep track of the number of users.
+     * Prints a confirmation message after the account is created.
+     */
     void create_new_account()
     {
         cout << "Please enter your user id: ";
@@ -65,13 +108,20 @@ public:
         cout << "Thank You! Your account has been created!\n";
     }
 
+    /**
+     * Function to handle user login.
+     */
     void login()
     {
         cout << "Please enter your user id: ";
         cin >> id;
         cout << "Please enter your password: ";
         cin >> password;
+
+        // Variable to store the index of the user in the arrays
         int user_index = -1;
+
+        // Loop through the arrays to find a matching user id and password
         for (int i = 0; i < num_users; i++)
         {
             if (ids[i] == id && passwords[i] == password)
@@ -80,14 +130,20 @@ public:
                 break;
             }
         }
+
+        // If a matching user is found
         if (user_index != -1)
         {
             cout << "Access Granted!\n";
             char option;
+
+            // Loop to display the main menu and handle user options
             do
             {
                 printMainMenu();
                 cin >> option;
+
+                // Switch statement to handle different user options
                 switch (option)
                 {
                 case 'd':
@@ -103,6 +159,8 @@ public:
                     double amount;
                     cout << "Amount of withdrawal: $";
                     cin >> amount;
+
+                    // Check if the withdrawal amount is less than or equal to the user's balance
                     if (amount <= balances[user_index])
                     {
                         balances[user_index] -= amount;
@@ -133,13 +191,23 @@ public:
         }
         else
         {
-            cout << "******** LOGIN FAILED! ********\n";
+            cout << "************ LOGIN FAILED! ************\n";
+            cout << "**** INVALID USER ID OR PASSWORD! ****\n";
         }
     }
 };
+/**
+ * @file main.cpp
+ * @brief This file contains the main function to start the program.
+ */
+
+/**
+ * @brief The main function of the program.
+ * @return 0 indicating successful execution of the program.
+ */
 int main()
 {
-    Bank obj;
-    obj.start();
-    return 0;
+    Bank obj;    // Create an instance of the Bank class.
+    obj.start(); // Call the start function of the Bank object.
+    return 0;    // Return 0 to indicate successful execution of the program.
 }
